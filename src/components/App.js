0066c0b079ff57d2c4867c3b92f45ef83e9f6348
codingclueless <codingclueless@gmail.com>
2019-08-18 21:17:30 -0700

    completed the api render with props. need css

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import styles from './App.css';
+import Recipe from './Recipe';
 
 const App = () => {
   const APP_ID = '6e0bc073';
   const APP_KEY = 'b9227dd5411a2b229de23edaf420a162';
 
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
 
   const getRecipes = async() => {
-    const res = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await res.json();
-    console.log(data);
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
   };
 
   return (
     <div className={styles.App}>
-      <form className={styles.searchForm}>
-        <input type="text" className={styles.searchBar} />
+      <form className={styles.searchForm} onSubmit={getSearch}>
+        <input type="text" className={styles.searchBar} value={search} onChange={updateSearch}/>
         <button className={styles.searchButton}>Search</button>
       </form>
-      <h1>Working</h1>
+      {recipes.map(recipe => (
+        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients}/>
+      ))}
     </div>
   );
 
---
+++ src/components/Recipe.js
@@ -0,0 +1,28 @@
+import React, { } from 'react';
+import PropTypes from 'prop-types';
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map(ingredient => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>{calories}</p>
+      <img src={image} alt={title}/>
+    </div>
+  );
+};
+
+Recipe.propTypes = {
+  title: PropTypes.string.isRequired,
+  calories: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  ingredients: PropTypes.string.isRequired
+};
+
+export default Recipe;
