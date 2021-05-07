27f38e12092bb2cb44103ba00bb33cb15247c512
Wes Griffin <wesgriffincodes@gmail.com>
2021-05-07 14:48:28 -0700

    green

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const App = () => {
   }, []);
 
 
-  const getRecipes = async() => {
+  const getRecipe = async() => {
     const res = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await res.json();
     console.log(data);
